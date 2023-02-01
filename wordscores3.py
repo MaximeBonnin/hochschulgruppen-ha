3cccd4472aa597bbc66ec8ee526e43c4089c0976
Maxime Bonnin <maxime.bonnin@gmx.de>
2023-02-01 11:33:50 +0100

    first results

--- wordscores3.py
+++ wordscores3.py
@@ -12,14 +12,13 @@ opath = 'C:/Users/User/Desktop/Uni/WiSe 22-23/Empirische Demokratieforschung/Sem
 
 # hardcode your reference cases and their scores
 A_r = pd.DataFrame({'referenceCase1': 1.0, # these are just examples
-                   'referenceCase2': 5.0, 
+                   # 'referenceCase2': 5.0, 
                    'referenceCase3': 10.0},
                    index = ['score'])
 
 
 class Wordscores:
     def __init__(self, A_r: pd.DataFrame):
-        print("Initialized")
         self.A_r = A_r
 
 
@@ -60,7 +59,7 @@ class Wordscores:
         self.S_w = pd.DataFrame(self.F_wr.word)
         self.S_w['score'] = self.P_wr.dot(self.A_r.T)
         self.S_w.to_csv(opath + 'wordscores.csv', index = False)
-        print(self.S_w)
+        # print(self.S_w)
         # return self.S_w
 
 
@@ -72,7 +71,7 @@ class Wordscores:
 
         # frequency of each virgin text word, as a proportion of the total number of words in the virgin text
         self.F_wv = self.load_data(virginSet, ipath, {0: 'S30', 2: 'float'})
-        print(self.F_wv)
+        # print(self.F_wv)
 
     
     def run(self):
